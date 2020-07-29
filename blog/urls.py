dfc8ae7b089747a3ee52799fd5b7a1c79ce9871c
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2020-07-29 15:51:21 +0530

    Updated Models, uses slugfiled instead of pk

--- blog/urls.py
+++ blog/urls.py
@@ -5,13 +5,13 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('',  views.IndexView.as_view(), name="index"),
     path('post/', views.PostListView.as_view(), name='post_list'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
     path('search/', views.SearchView.as_view(), name='search'),
     path('post/create/', login_required(views.PostCreateView.as_view()),
          name='post_create'),
-    path('post/update/<int:pk>/',
+    path('post/update/<slug:slug>/',
          login_required(views.PostUpdateView.as_view()), name='post_update'),
-    path('post/delete/<int:pk>/',
+    path('post/delete/<slug:slug>/',
          login_required(views.PostDeleteView.as_view()), name='post_delete'),
 
 ]
