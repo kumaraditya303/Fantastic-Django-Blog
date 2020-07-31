7b134fdfda4548a47271202a56f8b89e8a4607a1
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2020-07-31 15:41:20 +0530

    Updated settings

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Fantastic Django Blog
+
+[![Build Status](https://travis-ci.com/kumaraditya303/Fantastic-Django-Blog.svg?token=Tp128txvcHsePdipY3xq&branch=master)](https://travis-ci.com/kumaraditya303/Fantastic-Django-Blog)[![Dependabot Status](https://api.dependabot.com/badges/status?host=github&repo=kumaraditya303/Fantastic-Django-Blog&identifier=280406434)](https://dependabot.com)![Heroku](https://www.herokucdn.com/deploy/button.svg)![Stars](https://img.shields.io/github/stars/kumaraditya303/Fantastic-Django-Blog?style=social)
+
+> Fantastic Blog App made with 💖 in Django.
--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -187,11 +187,11 @@ EMAIL_HOST_USER = 'messmgmtsystem@gmail.com'
 EMAIL_HOST_PASSWORD = 'pythonflask'
 
 
-AWS_ACCESS_KEY_ID = 'AKIA3X3NMFVKT3Y54E4I'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'django-blog-staticfiles'
-AWS_S3_REGION_NAME = 'ap-south-1'
-AWS_IS_GZIPPED = True
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_ACCESS_KEY_ID = 'AKIA3X3NMFVKT3Y54E4I'
+# AWS_SECRET_ACCESS_KEY = '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'
+# AWS_STORAGE_BUCKET_NAME = 'django-blog-staticfiles'
+# AWS_S3_REGION_NAME = 'ap-south-1'
+# AWS_IS_GZIPPED = True
+# AWS_DEFAULT_ACL = None
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
