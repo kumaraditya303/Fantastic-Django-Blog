041385fd3c3656b4e16f082cc1886b69366dc73b
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2020-08-05 10:46:53 +0530

    Initial commit

---
+++ accounts/forms.py
@@ -0,0 +1,33 @@
+from accounts.models import Author
+from django.contrib.auth.models import User
+from django import forms
+
+from django.contrib.auth.forms import UserCreationForm
+
+
+class AuthorForm(forms.ModelForm):
+    picture = forms.ImageField(required=False)
+
+    class Meta:
+        model = Author
+        fields = ('picture',)
+
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'password1', 'password2')
+
+
+class UserUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name')
---
+++ blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from blog import views
+from django.contrib.auth.decorators import login_required
+urlpatterns = [
+    path('',  views.IndexView.as_view(), name="index"),
+    path('post/', views.PostListView.as_view(), name='post_list'),
+    path('post/<slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('search/', views.SearchView.as_view(), name='search'),
+    path('posts/create/', login_required(views.PostCreateView.as_view()),
+         name='post_create'),
+    path('posts/update/<slug>/',
+         login_required(views.PostUpdateView.as_view()), name='post_update'),
+    path('posts/delete/<slug>/',
+         login_required(views.PostDeleteView.as_view()), name='post_delete'),
+
+]
