00b6912bddc99b89ee49fe032d23a38d01232fb8
Kumar Aditya <59607654+kumaraditya303@users.noreply.github.com>
2020-08-13 15:21:19 +0530

    Updated user Profile Page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 from accounts import views
+
 urlpatterns = [
-    path('accounts/register/', views.AuthorCreateView.as_view(),
-         name='accounts_register'),
-    path("accounts/update/",
-         login_required(views.AuthorUpdateView.as_view()), name="accounts_update")
+    path(
+        "accounts/register/", views.AuthorCreateView.as_view(), name="accounts_register"
+    ),
+    path(
+        "accounts/profile/",
+        login_required(views.AuthorUpdateView.as_view()),
+        name="accounts_update",
+    ),
 ]
